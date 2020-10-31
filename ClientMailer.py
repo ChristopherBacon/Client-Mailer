8dc4e0c19e77969003f043266880f8de9a0e4944
ChristopherBacon <chrisjbacon85@gmail.com>
2020-10-31 17:45:29 +0000

    Comment added

--- ClientMailer.py
+++ ClientMailer.py
@@ -1,53 +1,39 @@
 
+'''
+PROJECT
+Client Mailer
+Small CRM
+emailer
+'''
 
 '''
+JOBS
 1. Get Client Data
-2. create class / database not sure as yet
+2. create client class
 3. test send email
 4. create library of email messages
 5. create scheduler to determine mailsend.
 
 '''
 
-import pandas as pd
-import csv
+class Client():
+	def __init__(self, first, last, job, company, email, replies=0, searches=0):
+		self.first = first
+		self.last = last
+		self.job = job
+		self.company = company
+		self.email = email
+		self.replies = replies
+		self.searches = searches
+
+	def __repr__(self):
+		return f"Name: {self.first} {self.last} \
+			   Job: {self.job} \
+			   Company: {self.company} \
+			   email: {self.email}"
+
+c1 = Client("Steve", "Winner" ,"Editor", "BBC", "steve.winner@bbc.co.uk")
 
 
-def main():
-	get_clients2()
-	print('test first')
-
-	#Getting better thanks Tommy
-
-#filepath = [r'/Users/christopherbacon/Downloads/jstdatasetr4.csv']
-#def get_clients():
-	#with open(filepath) as f:
-		#rows = f.reader(csv, delimiter=',')
-		#data = []
-		#for row in rows:
-			#data.append(row)
-filepath = '/Users/christopherbacon/Downloads/jstdatasetr4.csv'
-def get_clients2():
-	df = pd.read_csv(filepath)
-	print(df)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-	main()
+print(c1)
 
