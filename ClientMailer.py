2cd8e9f7b3e467159bd487a0bed1ff3fd2b9c9cf
ChristopherBacon <chrisjbacon85@gmail.com>
2020-11-06 22:00:42 +0000

    Trendy Funcs

--- ClientMailer.py
+++ ClientMailer.py
@@ -1,4 +1,3 @@
-
 '''
 PROJECT
 Client Mailer
@@ -16,50 +15,57 @@ JOBS
 
 '''
 
+
 class Client():
-	''' Client class, create clients.'''
-	def __init__(self, first, last, job, company, email, replies=0, searches=0):
-		self.first = first
-		self.last = last
-		self.job = job
-		self.company = company
-		self.email = email
-		self.replies = replies
-		self.searches = searches
+    """ Client class, create clients."""
 
-	def __repr__(self):
-		''' Returns client object as client detail '''
-		return f"Name: {self.first} {self.last} \
-		Job: {self.job} \
-		Company: {self.company} \
-		email: {self.email}"
+    def __init__(self, first, last, job, company, email, replies=0, searches=0):
+        self.first = first
+        self.last = last
+        self.job = job
+        self.company = company
+        self.email = email
+        self.replies = replies
+        self.searches = searches
 
-def get_clients(job):
-	'''Get clients dependent on job'''
-	for x in clients:
-		if x.job == job:
-			return print(x)
+    def __str__(self):
+        """ Returns client object as client detail """
+        return f"Name: {self.first} {self.last} \
+            Job: {self.job} \
+            Company: {self.company} \
+            email: {self.email}"
+
 
 # TODO: This needs more work, not quite functioning and logic doesn't work
-def get_more_clients(*people):
-	'''Get lots of clients with different parameters'''
-	for x in clients:
-		if x.first == people or x.last == people:
-			return print(x)
-		else:
-			return print("Please try again, your search has returned no results.")
+def search_for_clients(client_list, search_term):
+    matched_clients = []
+    for client in client_list:
+        if client.first == search_term or client.last == search_term or \
+                client.job == search_term or client.company == search_term:
+            matched_clients.append(client)
+    return matched_clients
 
 
+def get_client_data():
+    c1 = Client("Steve", "Winner", "Editor", "BBC", "steve.winner@bbc.co.uk")
+    c2 = Client("John", "Baker", "Producer", "ITV", "john.baker@itv.com", 1, 0)
+    c3 = Client("Isabelle", "Smith", "Promo Producer", "ITV", "isabelle.smith@itv.com")
+    return [c1, c2, c3]
 
 
+def print_clients(client_list):
+    if len(client_list) == 0:
+        print("Please try again, your search has returned no results.")
+    else:
+        for client in client_list:
+            print(client)
 
-c1 = Client("Steve", "Winner" , "Editor", "BBC", "steve.winner@bbc.co.uk")
-c2 = Client("John", "Baker", "Producer", "ITV", "john.baker@itv.com", 1, 0)
-c3 = Client("Isabelle", "Smith", "Promo Producer", "ITV", "isabelle.smith@itv.com")
 
-clients = [c1,c2,c3]
+def main():
+    clients = get_client_data()
+    clients_from_itv = search_for_clients(clients, "ITV")
+    print_clients(clients_from_itv)
 
-get_more_clients('Jones')
-print(Client.__dict__)
-#get_clients('Promo Producer')
 
+if __name__ == "__main__":
+    main()
